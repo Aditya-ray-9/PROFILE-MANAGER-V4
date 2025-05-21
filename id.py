fb97f613a6162a4e2295a1726e7cf1d80a5649cd
Aditya Ray <adityaray12341234@gmail.com>
2025-05-21 20:52:06 +0530

    Create id.py

---
+++ id.py
@@ -0,0 +1,6 @@
+id = ""
+app_name = "Profile-Manager"
+app_version = "V3.1"
+git = """git init; git remote add origin https://github.com/Aditya-ray-9/PROFILE-MANAGER-V3.git; git add .; git commit -m "Overwrite remote with latest local version"; git branch -M main; git push -u origin main --force
+"""
+languages = ["HTML", "JS(JAVASCRIPT)", "TS(TYPESCRIPT)", "CSS(TAILWIND)", "PYTHON", "JSON", "TSX(REACT)", "PROFILE"]
