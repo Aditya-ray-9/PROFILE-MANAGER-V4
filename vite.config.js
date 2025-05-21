0c9794cbff9a6d26fdccb0c8a0c5210db48baa8d
Aditya Ray <adityaray12341234@gmail.com>
2025-05-21 21:06:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,18 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
+// Try to locate the correct entry point
 export default defineConfig({
   plugins: [react()],
-  root: 'client/src',
-  publicDir: 'client/public',
+  
+  // This should point to where your index.html is located
+  root: '.',
+  
   build: {
-    outDir: '../dist/client',
+    outDir: 'dist/client',
     emptyOutDir: true
   },
+  
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './client/src')
